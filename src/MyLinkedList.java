5c6e2360164837398e528579b58a7ea3b024c2b5
liazarylkasyn <lia.zharylkasyn@icloud.com>
2024-04-24 09:46:21 +0500

    commit

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,3 +1,12 @@
 package Lists;
 public class MyLinkedList<T> implements Lists.MyList {
+    @Override
+    public void add(Object item) {
+
+    }
+
+    @Override
+    public void add(Object item, int index) {
+
+    }
 }
--- src/MyList.java
+++ src/MyList.java
@@ -1,5 +1,5 @@
 package Lists;
-public interface MyList<T> {
+public interface MyList<T> extends Iterable {
     void add (T item);
     void add (T item, int index);
     void add (T item);
