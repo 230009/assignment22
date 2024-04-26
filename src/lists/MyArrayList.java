54cefff6059d889fbaf1f95e2f5482bd14ae7473
liazarylkasyn <lia.zharylkasyn@icloud.com>
2024-04-26 20:06:55 +0500

    Merge remote-tracking branch 'github/main'
    # Conflicts:
    #	src/MyArrayList.java
    #	src/MyList.java

--- src/lists/MyArrayList.java
+++ src/lists/MyArrayList.java
@@ -1,12 +1,22 @@
 package lists;
 import java.util.Iterator;
 public class MyArrayList<T> implements MyList<T>{
-    private static final int DEFAULT_SIZE = 5;
     private T[] arr;
     private int size;
+   //
+    public MyArrayList() {
+        this.arr = new T[10];
+        this.size = 0;
+    }
 
     @Override
     public void add(T item) {
+        ensureCapacity();
+        arr[size++] = item;
+    }
+
+    @Override
+    public void add(Object item, int index) {
 
     }
     //constructor
