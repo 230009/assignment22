a867648ec8db4ea2c788b309e444bc7bba9ed445
liazarylkasyn <lia.zharylkasyn@icloud.com>
2024-04-26 20:50:39 +0500

    Merge remote-tracking branch 'github/main'
    # Conflicts:
    #	src/MyArrayList.java
    #	src/MyList.java

--- src/lists/MyArrayList.java
+++ src/lists/MyArrayList.java
@@ -1,12 +1,18 @@
 package lists;
 import java.util.Iterator;
 public class MyArrayList<T> implements MyList<T>{
+    private static final int INITIAL_CAPACITY = 5;
     private T[] arr;
-    private int size;
+    private int size = 0;
    //
     public MyArrayList() {
-        this.arr = new T[10];
-        this.size = 0;
+        arr = (T[]) new Object [INITIAL_CAPACITY];
+    }
+    public MyArrayList(int initial_cpct){
+        if (initial_cpct < 0){
+            throw new IllegalArgumentException();
+        }
+        arr = (T[]) new Object [initial_cpct];
     }
 
     @Override
